6972c8e9da60a62807b82b75b94e77e4147d5c15
Roman Matviy <roman@matviy.pp.ua>
2025-02-22 16:30:22 +0200

    add new page blog

---
+++ src/app/blog/page.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Блог GetMon – Корисні статті про моніторинг",
+    description: "Читайте найновіші статті про моніторинг та аналіз даних...",
+};
+
+export default function Blog() {
+    return <h1>Блог</h1>;
+}
