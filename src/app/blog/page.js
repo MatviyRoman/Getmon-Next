272fc06083df6ef6936588ad71a654cb3613ce87
Roman Matviy <roman@matviy.pp.ua>
2025-02-22 17:30:19 +0200

    get blog posts

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,11 +5,23 @@ export const metadata = {
     description: "Читайте найновіші статті про моніторинг та аналіз даних...",
 };
 
-export default function Blog() {
+export default async function Blog() {
+    // Запит до публічного API JSONPlaceholder для отримання постів
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await res.json();
+
     return (
-    <>
-        <Navbar />
-        <h1>Blog</h1>
-    </>
+        <>
+            <Navbar />
+            <h1>Blog Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
