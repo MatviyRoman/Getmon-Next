75bdf1c721c29dfb349cc31ef8a9d3aca6e765ac
Roman Matviy <roman@matviy.pp.ua>
2025-04-03 12:39:54 +0300

    update stats block

--- src/components/blocks/StatsBlock.js
+++ src/components/blocks/StatsBlock.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import "./StatsBlock.css";
+// import React from 'react';
+import styles from "./StatsBlock.module.css";
 import Image from 'next/image';
 
 const StatsBlock = ({ stats }) => {
     return (
         <>
             {stats.map((stat, index) => (
-                <div className="stats-block" key={index}>
-                    <div className="stat-item">
-                        <div className="stat-line"></div>
-                        <div className="stat-icon"></div>
-                        <p className="stat-item-value">{stat.value}</p>
-                        <h2 className="stat-item-title">{stat.title}</h2>
-                        <Image className="stat-logo" src="/img/stat-logo.svg" width="68" height="51" alt={stat.title} />
+                <div className={styles['stats-block']} key={index}>
+                    <div className={styles['stat-item']}>
+                        <div className={styles['stat-line']}></div>
+                        <div className={styles['stat-icon']}></div>
+                        <p className={styles['stat-item-value']}>{stat.value}</p>
+                        <h2 className={styles['stat-item-title']}>{stat.title}</h2>
+                        <Image className={styles['stat-logo']} src="/img/stat-logo.svg" width={68} height={51} alt={stat.title} />
                     </div>
-                    <div className="stat-description">
+                    <div className={styles['stat-description']}>
                         <p>{stat.description}</p>
-                        <img className="stat-line" src="/img/line-mobile.svg" alt="line" />
+                        <img className={styles['stat-line']} src="/img/line-mobile.svg" alt="line" />
                     </div>
                 </div>
             ))}
