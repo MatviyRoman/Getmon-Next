20e0653ae5fbf40ba467d33d5c517aace8eccabc
Roman Matviy <roman@matviy.pp.ua>
2025-04-21 18:56:31 +0300

    update mock data

--- src/data/pageData.js
+++ src/data/pageData.js
@@ -14,8 +14,8 @@ export const mockPages = [
                 block: "simple",
                 title: "KLIMATYZACJA WROCŁAW – GETMON",
                 content: `
-            <p>Jeśli potrzebna Ci dobra klimatyzacja, Wrocław jest miastem, w którym działamy. Od lat instalujemy klimatyzację w różnych rodzajach na terenie Wrocławia i okolic. Dobra klimatyzacja to podstawa, szczególnie w domu czy w firmie, gdzie dobre warunki pracy zwiększają wydajność pracowników.</p>
-            <p>Jeśli nie wiesz, czy potrzebujesz klimatyzacji kasetonowej, split czy multisplit – zapraszamy do kontaktu. Mamy wieloletnie doświadczenie w tym temacie i na pewno nasi eksperci pomogą Ci wybrać klimatyzację idealną do Twojego biura czy mieszkania. Na samym początku warto dowiedzieć się czym właściwie jest klimatyzacja – dlatego zapraszamy do dalszego czytania.</p>`,
+                    <p>Jeśli potrzebna Ci dobra klimatyzacja, Wrocław jest miastem, w którym działamy. Od lat instalujemy klimatyzację w różnych rodzajach na terenie Wrocławia i okolic. Dobra klimatyzacja to podstawa, szczególnie w domu czy w firmie, gdzie dobre warunki pracy zwiększają wydajność pracowników.</p>
+                    <p>Jeśli nie wiesz, czy potrzebujesz klimatyzacji kasetonowej, split czy multisplit – zapraszamy do kontaktu. Mamy wieloletnie doświadczenie w tym temacie i na pewno nasi eksperci pomogą Ci wybrać klimatyzację idealną do Twojego biura czy mieszkania. Na samym początku warto dowiedzieć się czym właściwie jest klimatyzacja – dlatego zapraszamy do dalszego czytania.</p>`,
                 button_text: "Potrzebujesz klimatyzacji do biura? Zadzwoń do nas!",
                 phone_number: "+48 884 884 823",
             },
@@ -31,31 +31,31 @@ export const mockPages = [
                 type: "html",
                 block: "simple",
                 content: `
-                <h2>Klimatyzacja Wrocław – co musisz wiedzieć?</h2>
-                <p>Cieplejsze pory roku to czas, kiedy trudno wyobrazić sobie funkcjonowanie bez dobrze działającej klimatyzacji. Urządzenia zapewniające odpowiednią temperaturę oraz wilgotność znajdują powszechne zastosowanie w samochodach i pojazdach transportu publicznego, prywatnych mieszkaniach, sklepach lub innych lokalach usługowych, a także we wszelkiego rodzaju miejscach pracy, gdzie klimatyzacja tworzy warunki sprzyjające wykonywaniu obowiązków. W wielu zakładach czy fabrykach urządzenia tego typu są potrzebne także do zapewnienia odpowiedniej temperatury dla procesów produkcyjnych oraz poprawnego działania maszyn i komputerów.</p>
+                    <h2>Klimatyzacja Wrocław – co musisz wiedzieć?</h2>
+                    <p>Cieplejsze pory roku to czas, kiedy trudno wyobrazić sobie funkcjonowanie bez dobrze działającej klimatyzacji. Urządzenia zapewniające odpowiednią temperaturę oraz wilgotność znajdują powszechne zastosowanie w samochodach i pojazdach transportu publicznego, prywatnych mieszkaniach, sklepach lub innych lokalach usługowych, a także we wszelkiego rodzaju miejscach pracy, gdzie klimatyzacja tworzy warunki sprzyjające wykonywaniu obowiązków. W wielu zakładach czy fabrykach urządzenia tego typu są potrzebne także do zapewnienia odpowiedniej temperatury dla procesów produkcyjnych oraz poprawnego działania maszyn i komputerów.</p>
 
-                <p><b>Wybierając klimatyzację</b>, należy kierować się takimi czynnikami, jak wielkość czy przeznaczenie pomieszczenia, aby dobrać urządzenie, które w takich warunkach sprawdzi się najlepiej. Konsekwencją właściwej decyzji będzie wysoka efektywność oraz zadowolenie użytkowników. Do najbardziej istotnych parametrów klimatyzatorów zalicza się: moc, poziom hałasu, zużycie prądu, sposób montażu. Warto też zwrócić uwagę, czy dane urządzenie posiada jedynie funkcję ochładzania, czy także dogrzewania. Profesjonalne klimatyzatory zapewniają podwójne działanie. Jeszcze do niedawna spotykało się je wyłącznie w zakładach pracy, jednak od paru lat cieszą się popularnością także w mieszkaniach i domach. To efekt z jednej strony postępujących zmian klimatycznych, a z drugiej dążenia do zapewniania sobie jak największego komfortu w czasie odpoczynku.</p>
+                    <p><b>Wybierając klimatyzację</b>, należy kierować się takimi czynnikami, jak wielkość czy przeznaczenie pomieszczenia, aby dobrać urządzenie, które w takich warunkach sprawdzi się najlepiej. Konsekwencją właściwej decyzji będzie wysoka efektywność oraz zadowolenie użytkowników. Do najbardziej istotnych parametrów klimatyzatorów zalicza się: moc, poziom hałasu, zużycie prądu, sposób montażu. Warto też zwrócić uwagę, czy dane urządzenie posiada jedynie funkcję ochładzania, czy także dogrzewania. Profesjonalne klimatyzatory zapewniają podwójne działanie. Jeszcze do niedawna spotykało się je wyłącznie w zakładach pracy, jednak od paru lat cieszą się popularnością także w mieszkaniach i domach. To efekt z jednej strony postępujących zmian klimatycznych, a z drugiej dążenia do zapewniania sobie jak największego komfortu w czasie odpoczynku.</p>
 
-                <h2>Montaż klimatyzacji Wrocław</h2>
-                <p>Instalacja złożonych systemów klimatyzacji to zadanie wymagające pewnej wiedzy oraz doświadczenia. Niewłaściwe umieszczenie jednostki wewnętrznej lub zewnętrznej, albo nieodpowiednio wykonany odpływ powietrza i wilgoci, to tylko niektóre z najczęściej popełnianych błędów podczas samodzielnego montażu klimatyzacji. Rezultaty takich działań to przede wszystkim ryzyko częstych awarii, a nawet utrata gwarancji. W tym przypadku wysiłek włożony we własnoręczne wykonanie instalacji nie przyniesie korzyści finansowych, a wręcz przeciwnie, nie mówiąc już o małej efektywności źle zamontowanych urządzeń.</p>
+                    <h2>Montaż klimatyzacji Wrocław</h2>
+                    <p>Instalacja złożonych systemów klimatyzacji to zadanie wymagające pewnej wiedzy oraz doświadczenia. Niewłaściwe umieszczenie jednostki wewnętrznej lub zewnętrznej, albo nieodpowiednio wykonany odpływ powietrza i wilgoci, to tylko niektóre z najczęściej popełnianych błędów podczas samodzielnego montażu klimatyzacji. Rezultaty takich działań to przede wszystkim ryzyko częstych awarii, a nawet utrata gwarancji. W tym przypadku wysiłek włożony we własnoręczne wykonanie instalacji nie przyniesie korzyści finansowych, a wręcz przeciwnie, nie mówiąc już o małej efektywności źle zamontowanych urządzeń.</p>
 
-                <p><b>Najpewniejsze rozwiązanie</b>, aby uniknąć takich niepożądanych efektów, to zatrudnienie specjalistów, posiadającej niezbędne certyfikaty oraz uprawnienia. We Wrocławiu i okolicach montaż klimatyzacji mogą Państwo zlecić doświadczonej firmie Getmon. Nasze kompetencje w tej dziedzinie potwierdzają odpowiednie dokumenty, będące gwarancją bezpiecznego oraz sprawnego wykonania wszystkich prac związanych z instalacją klimatyzatorów. Stosując najnowsze standardy, potrafimy zapewnić Klientom całkowitą satysfakcję, zarówno z samego przebiegu realizacji, jak i późniejszej funkcjonalności systemu. Oprócz montażu klimatyzacji zakres naszych usług obejmuje przegląd, a także serwis klimatyzatorów każdego rodzaju.</p>
-                <div class="image-gallery">
-                    <img src="/img/section1.png" alt="Instalacja klimatyzacji" width="853" height="513">
-                </div>
-                <p>Zapraszamy do zapoznania się z pełną ofertą na naszej stronie internetowej, a w przypadku jakichkolwiek pytań związanych z działalnością firmy, prosimy o kontakt. Z przyjemnością udzielimy wszystkich informacji.</p>
+                    <p><b>Najpewniejsze rozwiązanie</b>, aby uniknąć takich niepożądanych efektów, to zatrudnienie specjalistów, posiadającej niezbędne certyfikaty oraz uprawnienia. We Wrocławiu i okolicach montaż klimatyzacji mogą Państwo zlecić doświadczonej firmie Getmon. Nasze kompetencje w tej dziedzinie potwierdzają odpowiednie dokumenty, będące gwarancją bezpiecznego oraz sprawnego wykonania wszystkich prac związanych z instalacją klimatyzatorów. Stosując najnowsze standardy, potrafimy zapewnić Klientom całkowitą satysfakcję, zarówno z samego przebiegu realizacji, jak i późniejszej funkcjonalności systemu. Oprócz montażu klimatyzacji zakres naszych usług obejmuje przegląd, a także serwis klimatyzatorów każdego rodzaju.</p>
+                    <div class="image-gallery">
+                        <img src="/img/section1.png" alt="Instalacja klimatyzacji" width="853" height="513">
+                    </div>
+                    <p>Zapraszamy do zapoznania się z pełną ofertą na naszej stronie internetowej, a w przypadku jakichkolwiek pytań związanych z działalnością firmy, prosimy o kontakt. Z przyjemnością udzielimy wszystkich informacji.</p>
 
-                <h2>Klimatyzacja MultiSplit – jak działa?</h2>
-                <p><b>Klimatyzatory typu multi split</b> to rodzaj klimatyzatorów wykorzystujących wiele parowników i sprężarek do chłodzenia wielu pomieszczeń lub obszarów. Zaletą systemu multi split w porównaniu z systemem pojedynczym jest to, że pozwala on na indywidualną regulację temperatury w każdym pomieszczeniu lub obszarze. Systemy typu multi split mogą być również bardziej energooszczędne niż systemy typu single split, ponieważ wymagają one jedynie pracy sprężarki i parownika, które aktywnie chłodzą dany obszar. Ponadto systemy multi split są zazwyczaj cichsze niż systemy single split, dzięki czemu idealnie nadają się do stosowania w sypialniach lub innych cichych pomieszczeniach. Jeśli szukasz uniwersalnego i wydajnego sposobu chłodzenia domu, klimatyzator typu multi split jest doskonałym rozwiązaniem.</p>
+                    <h2>Klimatyzacja MultiSplit – jak działa?</h2>
+                    <p><b>Klimatyzatory typu multi split</b> to rodzaj klimatyzatorów wykorzystujących wiele parowników i sprężarek do chłodzenia wielu pomieszczeń lub obszarów. Zaletą systemu multi split w porównaniu z systemem pojedynczym jest to, że pozwala on na indywidualną regulację temperatury w każdym pomieszczeniu lub obszarze. Systemy typu multi split mogą być również bardziej energooszczędne niż systemy typu single split, ponieważ wymagają one jedynie pracy sprężarki i parownika, które aktywnie chłodzą dany obszar. Ponadto systemy multi split są zazwyczaj cichsze niż systemy single split, dzięki czemu idealnie nadają się do stosowania w sypialniach lub innych cichych pomieszczeniach. Jeśli szukasz uniwersalnego i wydajnego sposobu chłodzenia domu, klimatyzator typu multi split jest doskonałym rozwiązaniem.</p>
 
-                <div class="image-gallery">
-                    <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
-                </div>
+                    <div class="image-gallery">
+                        <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
+                    </div>
 
-                <h2>Kiedy klimatyzacja multi split to najlepsze rozwiązanie?</h2>
-                <p>Klimatyzatory split doskonale sprawdzają się w przypadku pojedynczego pomieszczenia. Co jednak zrobić w przypadku, gdy chcemy korzystać z klimatyzacji w całym mieszkaniu lub biurze? Czy w każdym pokoju trzeba instalować osobne urządzenia? Na szczęście istnieje nieco tańsze rozwiązanie, znane jako multi split. W tym przypadku wystarczy tylko jedna jednostka zewnętrzna, do której można podłączyć kilka urządzeń wewnętrznych. Ich liczba zależy od modelu. Najczęściej pojedynczy agregat chłodzący może obsługiwać do pięciu pomieszczeń.</p>
+                    <h2>Kiedy klimatyzacja multi split to najlepsze rozwiązanie?</h2>
+                    <p>Klimatyzatory split doskonale sprawdzają się w przypadku pojedynczego pomieszczenia. Co jednak zrobić w przypadku, gdy chcemy korzystać z klimatyzacji w całym mieszkaniu lub biurze? Czy w każdym pokoju trzeba instalować osobne urządzenia? Na szczęście istnieje nieco tańsze rozwiązanie, znane jako multi split. W tym przypadku wystarczy tylko jedna jednostka zewnętrzna, do której można podłączyć kilka urządzeń wewnętrznych. Ich liczba zależy od modelu. Najczęściej pojedynczy agregat chłodzący może obsługiwać do pięciu pomieszczeń.</p>
 
-                <p>Warto wiedzieć, że każdy z podłączonych w ten sposób klimatyzatorów ściennych będzie działał niezależnie od pozostałych. Istnieje możliwość indywidualnej regulacji temperatury do potrzeb osób znajdujących się w poszczególnych pokojach. Klimatyzacja multi split sprawdzi się zatem wszędzie tam, gdzie istnieje potrzeba instalacji kilku klimatyzatorów, a mniejsza liczba urządzeń zewnętrznych pozwoli zmniejszyć koszty oraz oszczędzić miejsce.</p>
+                    <p>Warto wiedzieć, że każdy z podłączonych w ten sposób klimatyzatorów ściennych będzie działał niezależnie od pozostałych. Istnieje możliwość indywidualnej regulacji temperatury do potrzeb osób znajdujących się w poszczególnych pokojach. Klimatyzacja multi split sprawdzi się zatem wszędzie tam, gdzie istnieje potrzeba instalacji kilku klimatyzatorów, a mniejsza liczba urządzeń zewnętrznych pozwoli zmniejszyć koszty oraz oszczędzić miejsce.</p>
                 `,
             },
             {
@@ -98,8 +98,8 @@ export const mockPages = [
                 block: "simple",
                 title: "KLIMATYZACJA WROCŁAW – GETMON",
                 content: `
-            <p>Jeśli potrzebna Ci dobra klimatyzacja, Wrocław jest miastem, w którym działamy. Od lat instalujemy klimatyzację w różnych rodzajach na terenie Wrocławia i okolic. Dobra klimatyzacja to podstawa, szczególnie w domu czy w firmie, gdzie dobre warunki pracy zwiększają wydajność pracowników.</p>
-            <p>Jeśli nie wiesz, czy potrzebujesz klimatyzacji kasetonowej, split czy multisplit – zapraszamy do kontaktu. Mamy wieloletnie doświadczenie w tym temacie i na pewno nasi eksperci pomogą Ci wybrać klimatyzację idealną do Twojego biura czy mieszkania. Na samym początku warto dowiedzieć się czym właściwie jest klimatyzacja – dlatego zapraszamy do dalszego czytania.</p>`,
+                    <p>Jeśli potrzebna Ci dobra klimatyzacja, Wrocław jest miastem, w którym działamy. Od lat instalujemy klimatyzację w różnych rodzajach na terenie Wrocławia i okolic. Dobra klimatyzacja to podstawa, szczególnie w domu czy w firmie, gdzie dobre warunki pracy zwiększają wydajność pracowników.</p>
+                    <p>Jeśli nie wiesz, czy potrzebujesz klimatyzacji kasetonowej, split czy multisplit – zapraszamy do kontaktu. Mamy wieloletnie doświadczenie w tym temacie i na pewno nasi eksperci pomogą Ci wybrać klimatyzację idealną do Twojego biura czy mieszkania. Na samym początku warto dowiedzieć się czym właściwie jest klimatyzacja – dlatego zapraszamy do dalszego czytania.</p>`,
                 button_text: "Potrzebujesz klimatyzacji do biura?",
             },
             {
@@ -107,14 +107,15 @@ export const mockPages = [
                 type: "html",
                 block: "simple",
                 content: `
-            <div class="image-gallery">
-                <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
-                <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
-                <div class="gallery-caption">
-                    <h3>Nasze realizacje</h3>
-                    <p>Profesjonalny montaż klimatyzacji w biurach i mieszkaniach</p>
-                </div>
-            </div>`,
+                    <div class="image-gallery">
+                        <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
+                        <img src="/img/section2.png" alt="Serwis klimatyzacji" width="853" height="513">
+                        <div class="gallery-caption">
+                            <h3>Nasze realizacje</h3>
+                            <p>Profesjonalny montaż klimatyzacji w biurach i mieszkaniach</p>
+                        </div>
+                    </div>
+                `,
             },
         ],
     },
