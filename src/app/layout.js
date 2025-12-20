f4db11e868af241b9e65982070d621bdf24952fd
Roman Matviy <roman@matviy.pp.ua>
2025-12-20 14:43:15 +0200

    added google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -106,8 +106,27 @@ export default function RootLayout({ children }) {
         {/* <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" /> */}
         {/* <meta property="article:modified_time" content="2023-03-11T14:53:48+00:00" /> */}
         <meta name="google-site-verification" content="99RlA_3wOmqBTKgzLHvHZVDkup8MRCWjPPqEuab81RE" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPBL4BL');`
+        }} />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${poppins.variable} ${roboto.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MPBL4BL"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         {children}
         <Footer />
